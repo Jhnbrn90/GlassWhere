09cb7d34a54fa144fb45eac852b6f77259549c13
John Braun <jbraunnl@gmail.com>
2018-12-10 23:22:56 +0100

    Add split into labs

--- app/Http/Controllers/GlassWareController.php
+++ app/Http/Controllers/GlassWareController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Glassware;
+use App\Lab;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -19,15 +20,21 @@ class GlassWareController extends Controller
           return view('login');
         }
 
-        $categories = [
-          'Roundbottoms' => Glassware::where('type', 'roundbottom')->get(),
-          'Erlenmeyers'  => Glassware::where('type', 'erlenmeyer')->get(),
-          'Separation funnels'  => Glassware::where('type', 'separation funnel')->get(),
-          'Measuring cylinders'  => Glassware::where('type', 'measuring cylinder')->get(),
-          'Beakers'  => Glassware::where('type', 'beaker')->get(),
-        ];
-        
-        return view('glassware.index', compact('categories'));
+
+
+        foreach (Lab::all() as $lab) {
+          $categories[$lab->name] = [
+            'Roundbottoms' => Glassware::where('type', 'roundbottom')->lab($lab)->get(),
+            'Erlenmeyers'  => Glassware::where('type', 'erlenmeyer')->lab($lab)->get(),
+            'Separation funnels'  => Glassware::where('type', 'separation funnel')->lab($lab)->get(),
+            'Measuring cylinders'  => Glassware::where('type', 'measuring cylinder')->lab($lab)->get(),
+            'Beakers'  => Glassware::where('type', 'beaker')->lab($lab)->get(),
+          ];  
+        }
+
+        $labs = Lab::all()->pluck('name');
+
+        return view('glassware.index', compact('categories', 'labs'));
     }
 
     public function show(Glassware $glassware)
