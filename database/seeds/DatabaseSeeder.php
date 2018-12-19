b0e5f788f5aa3e54d7e4c44ccc4b5726e19b803a
John Braun <jbraunnl@gmail.com>
2018-12-19 12:20:39 +0100

    Add extra volume options

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -25,14 +25,14 @@ class DatabaseSeeder extends Seeder
           '250 mL',
           '500 mL',
           '1 L',
-          '2 L'
+          '2 L',
         ];
 
         $this->createGlasswareForVolumes('roundbottom', $volumes);
-        $this->createGlasswareForVolumes('erlenmeyer', $volumes);
-        $this->createGlasswareForVolumes('separation funnel', $volumes);
+        $this->createGlasswareForVolumes('erlenmeyer', array_merge($volumes, ['3 L']));
+        $this->createGlasswareForVolumes('separation funnel', array_merge($volumes, ['3 L']));
         $this->createGlasswareForVolumes('measuring cylinder', $volumes);
-        $this->createGlasswareForVolumes('beaker', $volumes);
+        $this->createGlasswareForVolumes('beaker', array_merge($volumes, ['3 L']));
         
         $this->createGlasswareForVolumes(
           'dropping funnel', 
@@ -51,7 +51,7 @@ class DatabaseSeeder extends Seeder
         
         $this->createGlasswareForVolumes(
           'schlenk', 
-          ['10 mL', '25 mL', '50 mL', '100 mL', '250 mL', '500 mL']
+          ['10 mL', '25 mL', '50 mL', '100 mL', '250 mL', '500 mL', '1 L']
         );
         
         $this->createGlasswareForVolumes(
@@ -61,7 +61,7 @@ class DatabaseSeeder extends Seeder
 
         $this->createGlasswareForVolumes(
           'column', 
-          ['extra small', 'small', 'medium', 'large', 'extra large', 'big berta']
+          ['< 1 cm', '1 - 3 cm', '3 - 4 cm', '4 - 6 cm', '6 - 10 cm', '10+ cm']
         );
         
         $this->createGlasswareForVolumes('schlenk-adapter', ['NS14', 'NS29', 'other']);
